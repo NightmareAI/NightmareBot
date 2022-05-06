16ea50aee99e9db606aaf92973ef3d06e90dee6a
Stephan Auerhahn <palp@palp.org>
2022-05-05 20:35:21 -0700

    Queue fixes

--- Controllers/ResultController.cs
+++ Controllers/ResultController.cs
@@ -25,31 +25,42 @@ public class ResultController : ControllerBase
     [HttpPost]
     public async Task<ActionResult> LatentDiffusionResponse(ResponseModel response, [FromServices] DaprClient daprClient, [FromServices] DiscordSocketClient discordClient) 
     {
-        // Retrieve request from state store by ID
-        var request =
-            await daprClient.GetStateAsync<PredictionRequest<LatentDiffusionInput>>("request-state",
-                response.id.ToString());
-        
-        _logger.LogInformation($"Context: Guild {request.context.guild} Channel {request.context.channel} Message {request.context.message} User {request.context.user}");
-        var guild_id = ulong.Parse(request.context.guild);
-        var guild = discordClient.GetGuild(guild_id);
-        if (guild == null)
+        try
         {
-            _logger.LogWarning("Unable to get guild from discord");
+            // Retrieve request from state store by ID
+            var request =
+                await daprClient.GetStateAsync<PredictionRequest<LatentDiffusionInput>>("request-state",
+                    response.id.ToString());
+
+            _logger.LogInformation(
+                $"Context: Guild {request.context.guild} Channel {request.context.channel} Message {request.context.message} User {request.context.user}");
+            var guild_id = ulong.Parse(request.context.guild);
+            var guild = discordClient.GetGuild(guild_id);
+            if (guild == null)
+            {
+                _logger.LogWarning("Unable to get guild from discord");
+                return BadRequest();
+            }
+
+            var channel_id = ulong.Parse(request.context.channel);
+            var channel = guild.GetTextChannel(channel_id);
+            var messageReference = new MessageReference(ulong.Parse(request.context.message), channel_id, guild_id);
+
+            var message =
+                $"> {request.input.prompt}\n(latent-diffusion, {(DateTime.UtcNow - request.request_time).TotalSeconds} seconds end to end)\n";
+            foreach (var sample in response.images)
+            {
+                message += $"https://dumb.dev/nightmarebot-output/{response.id}/samples/{sample}\n";
+            }
+
+            await channel.SendMessageAsync(message, messageReference: messageReference);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex,"Failed to respond");
             return BadRequest();
         }
-        var channel_id = ulong.Parse(request.context.channel);
-        var channel = guild.GetTextChannel(channel_id);
-        var messageReference = new MessageReference(ulong.Parse(request.context.message), channel_id, guild_id);
-
-        var message = $"> {request.input.prompt}\n(latent-diffusion, {(DateTime.UtcNow - request.request_time).TotalSeconds} seconds end to end)\n";
-        foreach (var sample in response.images)
-        {
-            message += $"https://dumb.dev/nightmarebot-output/{response.id}/samples/{sample}\n";
-        }
-        
-        await channel.SendMessageAsync(message, messageReference: messageReference);
-        return Ok();
     }
 
 
